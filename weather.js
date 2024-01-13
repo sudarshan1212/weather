703d521812047c6e4fbf37440d09ecce9e4e325c
shyam2323232323232 <131328505+shyam2323232323232@users.noreply.github.com>
2024-01-14 00:44:38 +0530

    added images

--- weather.js
+++ weather.js
@@ -11,8 +11,8 @@ const response=await fetch(apiUrl+city+`&appid=${apiKey}`)
 if(response.status==404){
     // document.querySelectorAll(".col").style.display="none"
     none([".left",".right",".temp"])
-    weathericon.src="./images/search.png"
-    document.querySelector(".city").innerText="Invalid City Name"
+    weathericon.src="./images/404.png"
+    document.querySelector(".city").innerText="Oops! Location not found!"
     
   
 }else{
@@ -23,14 +23,19 @@ if(response.status==404){
     document.querySelector(".wind").innerText=data.wind.speed+' Km/h'
     show([".left",".right",".temp",])
    if(data.weather[0].main=="Clouds"){
+    document.body.style.backgroundImage="url('./images/sunny.jpeg')" 
     weathericon.src="./images/clouds.png"
    }else if(data.weather[0].main=="Clear"){
-    weathericon.src="./images/clouds.png"
+    document.body.style.backgroundImage="url('./images/sunny.jpg')"
+    weathericon.src="./images/clear.png"
    }else if(data.weather[0].main=="Rain"){
+    document.body.style.backgroundImage="url('./images/raining.png.jpg')"
     weathericon.src="./images/rain.png"
    }else if(data.weather[0].main=="Drizzle"){
+    document.body.style.backgroundImage="url('./images/sunny.jpg')"
     weathericon.src="./images/drizzle.png"
    }else if(data.weather[0].main=="Mist"){
+    document.body.style.backgroundImage="url('./images/sunny.jpg')"
     weathericon.src="./images/mist.png"
    }
 }
@@ -38,12 +43,14 @@ if(response.status==404){
 }
 
  async function init(){
+   
     const response=await fetch(apiUrl+"chennai"+`&appid=${apiKey}`)
     var data=await response.json()
     document.querySelector(".city").innerText=data.name
     document.querySelector(".temp").innerText=Math.floor(data.main.temp)+'°C'
     document.querySelector(".humidity").innerText=data.main.humidity+'%'
     document.querySelector(".wind").innerText=data.wind.speed+' Km/h'
+     document.body.style.backgroundImage="url('./images/sunny.jpg')" 
 
 }
 
